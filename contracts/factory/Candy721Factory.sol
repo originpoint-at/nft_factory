15c60e6e85a1e980cb4200318d2945a19639c323
buffalo <nobody@nothing.com>
2023-10-24 20:04:41 +0800

    modify

---
+++ contracts/factory/Candy721Factory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Candy721Ownable.sol";
+
+contract Candy721Factory {
+    // 保存所有已经创建的NFT合约地址
+    address[] public contracts;
+    address[] public creators;
+
+    // 事件，用于通知新合约的创建
+    event ContractCreated(address indexed nftContract, address indexed owner);
+
+    // 创建一个新的NFT合约
+    function createContract(string memory name, string memory symbol) external {
+        Candy721Ownable nft = new Candy721Ownable(name, symbol);
+        contracts.push(address(nft));
+        creators.push(msg.sender);
+        nft.transferOwnership(msg.sender);
+
+        emit ContractCreated(address(nft), msg.sender);
+    }
+
+    // 获取已创建的NFT合约数量
+    function contractAmount() external view returns (uint256) {
+        return contracts.length;
+    }
+}
--- contracts/factory/Candy721Ownable.sol
+++ contracts/factory/Candy721Ownable.sol
@@ -4,20 +4,27 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC721Ownable is ERC721, Ownable {
+contract Candy721Ownable is ERC721, Ownable {
 
     uint256 public tokenCount;
 
     mapping(uint256 => string) private tokenImage;
 
+    mapping(uint256 => string) private tokenSignature;
+
+    mapping(string => address) private signatureOwner;
+
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
 
     }
 
-    function mint(address to, string calldata image) public virtual {
+    function mint(address to, string calldata signature, string calldata image) public virtual {
         uint256 tokenId = tokenCount++;
+        // _mint requires to != address(0)
         _mint(to, tokenId);
         tokenImage[tokenId] = image;
+        tokenSignature[tokenId] = signature;
+        signatureOwner[signature] = to;
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
